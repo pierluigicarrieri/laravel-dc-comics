a5f2b10e4140d3a648c817d7054e73e2fe58fdbc
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-09-30 16:54:22 +0200

    Working on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.public')
 
-    
-</body>
-</html>
+@section('content')
+
+<main>
+
+    <h1>ciaoooooooo questo è il contenuto del main, che sarà (spero a breve) sostituito da quei cazzo dei fumetti</h1>
+
+
+</main>
+
+@endsection
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/scss/app.scss', 'resources/js/app.js']) 
-    <title>Document</title>
+    <title>@yield('title')</title>
 </head>
 <body>
+
+    @include('partials.header')
+
+    @yield('content')
+
+    @include('partials.footer')
     
 </body>
 </html>
