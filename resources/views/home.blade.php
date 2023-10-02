4a80a921b6da10519c35d711b6919e44611c3cd3
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-10-02 10:09:34 +0200

    Added homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.public')
+
+@section('title', 'Comics')
+
+@section('content')
+
+<main class="py-5">
+
+    <div class="container">
+
+        <h1 class="py-5 text-center">Welcome to the Hompage! To view comics click on Comics link on the top nav</h1>
+
+    </div>
+
+</main>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
